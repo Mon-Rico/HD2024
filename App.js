b3f0d307c0dcc470fbf4dae7f8a9fcc4ed44a2a2
Mon-Rico <115199569+Mon-Rico@users.noreply.github.com>
2024-04-27 14:39:50 -0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,27 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <button type="button" onclick="document.write(5 + 6)">Try it</button>
+
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  
+  
+});
+
+
+
